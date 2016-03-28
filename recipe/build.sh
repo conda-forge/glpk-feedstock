c9dc48d807b2ab4af4781f202a29bd8f588889e4
Jonathan Helmus <jjhelmus@gmail.com>
2016-03-28 11:03:10 -0500

    Set LD_LIBRARY_PATH to build.sh
    [skip appveyor]

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 export LDFLAGS="-L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 ./configure --prefix=$PREFIX --with-gmp
 
 make
