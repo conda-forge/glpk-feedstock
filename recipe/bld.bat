caf12f6466b0e037d79b3841badde58d52a53980
Joshua Arnott <josh@snorfalorpagus.net>
2016-08-24 17:39:29 +0100

    Build shared library (DLL) on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,9 +2,12 @@ cd w%ARCH%
 copy config_VC config.h
 
 nmake /f Makefile_VC
+nmake /f Makefile_VC_DLL
 
 copy glpsol.exe %LIBRARY_BIN%
 copy ..\src\glpk.h %LIBRARY_INC%
 copy glpk.lib %LIBRARY_LIB%
+copy glpk_4_60.dll %LIBRARY_BIN%
+copy glpk_4_60.dll %LIBRARY_BIN%\glpk.dll
 
 if errorlevel 1 exit 1
