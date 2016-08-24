de8187e29b57b5c0b8d9947f7996c60ebcf5c884
Joshua Arnott <josh@snorfalorpagus.net>
2016-08-24 18:23:50 +0100

    Make bld.bat robust to version changes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,17 @@
 cd w%ARCH%
 copy config_VC config.h
 
+:: Get the major minor version info (e.g. `4_60`)
+python -c "import os; print('_'.join(os.environ['PKG_VERSION'].split('.')[:2]))" > temp.txt
+set /p MAJ_MIN_VER=<temp.txt
+
 nmake /f Makefile_VC
 nmake /f Makefile_VC_DLL
 
 copy glpsol.exe %LIBRARY_BIN%
 copy ..\src\glpk.h %LIBRARY_INC%
 copy glpk.lib %LIBRARY_LIB%
-copy glpk_4_60.dll %LIBRARY_BIN%
-copy glpk_4_60.dll %LIBRARY_BIN%\glpk.dll
+copy glpk_%MAJ_MIN_VER%.dll %LIBRARY_BIN%
+copy glpk_%MAJ_MIN_VER%.dll %LIBRARY_BIN%\glpk.dll
 
 if errorlevel 1 exit 1
