2515070f4011b42c5230ed066ed27dfa7a218790
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-09-10 14:29:54 -0400

    Stop messing with LD_LIBRARY_PATH
    This going to cause some issues on newer version of macOS (e.g. 10.12).
    So we simply cannot allow setting the `LD_LIBRARY_PATH`. Hence it is
    dropped. Though there may be other changes necessitated by this
    adjustment.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,6 @@
 export LDFLAGS="-L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
 
-if [ "$(uname)" == "Darwin" ];
-then
-    export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
-fi
-
 ./configure --prefix=$PREFIX --with-gmp
 
 make
