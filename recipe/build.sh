77104cfefbc3912a1ae6a936ffaeabccd77feaea
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-09-10 14:50:08 -0400

    Set rpath on macOS and Linux
    Necessary to link gmp correctly and run the tests without linkage
    issues.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,14 @@
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
 
+if [ "$(uname)" == "Darwin" ];
+then
+    export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
+elif [ "$(uname)" == "Linux" ];
+then
+    export LDFLAGS="${LDFLAGS} -Wl,-rpath=${PREFIX}/lib"
+fi
+
 ./configure --prefix="${PREFIX}" --with-gmp
 
 make
