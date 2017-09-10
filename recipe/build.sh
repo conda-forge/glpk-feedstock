3f89eb6bb265aa26c3b0b9e021974f4d945c8aa7
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-09-10 14:31:08 -0400

    Quote the PREFIX
    If one builds with this recipe offline and there are spaces in the
    PREFIX, we want to be sure to keep the path intact. Thus we now quote
    it.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 export LDFLAGS="-L${PREFIX}/lib"
 export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
 
-./configure --prefix=$PREFIX --with-gmp
+./configure --prefix="${PREFIX}" --with-gmp
 
 make
 make check
