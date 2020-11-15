ede8fa7f702e3f4e14dc764d5522960e8221a0c6
Isuru Fernando <isuruf@gmail.com>
2020-11-15 10:25:44 -0600

    clean up

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,23 +2,10 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
-export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
-export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
-
-if [ "$(uname)" == "Darwin" ];
-then
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
-elif [ "$(uname)" == "Linux" ];
-then
-    export LDFLAGS="${LDFLAGS} -Wl,-rpath=${PREFIX}/lib"
-fi
-
-cp ${RECIPE_DIR}/config.guess .
-
 ./configure --prefix="${PREFIX}" --with-gmp
 
-make
+make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+  make check
 fi
 make install
